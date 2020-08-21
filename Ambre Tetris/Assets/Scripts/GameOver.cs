3634b6d71defdfd6c33a3622b289a0e089b5eb05
ajkrienke <audiocrunk@gmail.com>
2020-08-21 14:55:04 -0400

    added game over options and preview game piece in main game

--- Ambre Tetris/Assets/Scripts/GameOver.cs
+++ Ambre Tetris/Assets/Scripts/GameOver.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameOver : MonoBehaviour
@@ -7,7 +7,7 @@ public class GameOver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
---
+++ Ambre Tetris/Assets/Scripts/RestartScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestartScript : MonoBehaviour
+{
+    public Button restartButton;
+    public Button quitButton;
+    void Start () {
+		Button btn1 = restartButton.GetComponent<Button>();
+		btn1.onClick.AddListener(Restart);
+        Button btn2 = quitButton.GetComponent<Button>();
+		btn2.onClick.AddListener(Quit);
+	}
+
+	void Restart(){
+		SceneManager.LoadScene("Main Screen");
+	}
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
